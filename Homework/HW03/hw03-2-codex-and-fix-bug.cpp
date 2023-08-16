33e592740806b92fc56212c27644f07d86bada2f
zarlipzz <guitar30255@gmail.com>
2023-08-17 01:12:57 +0700

    ghghghgh

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -1,28 +1,17 @@
 //TODO: #9 Fix bug and arrange code in CODEX pattern.
-#include <stdio.h>
-
+#include <stdio.h> 
 int main() {
-    float a;
-    float b = 3;
-    bool _1 = 0;
-    printf("%d ", _1);
-    
-    int i = 0;
-    if (_1 != 0) {
-        printf("ok ");
-    } else {
-        while (i <= 5) {
-            printf("not-okey-%d ", ++i);
-        }
-    }
-
-    for (i = 3; i < 10; i += 1) {
-        if (i % 2 == (!_1 ? 1 : 2)) {
-            printf("see see");
-        } else {
-            printf("haha");
-        }
-    }
-
-    return 0;
+    float a, b = 3 ; 
+    bool _1 = 0 ; 
+    printf( "%d ",_1 ) ; 
+    int i = 0 ;
+    if( _1 != 0 )
+    printf( "ok " ) ; 
+    else while( i <= 5 ) 
+    printf( " not-okey-%d ", ++i ); 
+    for( i = 3 ; i < 10 ; i += 1 ) 
+    if( i % 2 == ( !1 ?1 :2 ) ) 
+    printf( "see see" ) ; 
+    printf( "haha" ) ;
+    return 0 ;
 }
